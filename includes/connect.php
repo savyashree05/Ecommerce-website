f5e670468f71b8a5366105597f87710b19b46eee
Savya Shree S <savyashree05@gmail.com>
2024-07-15 22:55:27 +0530

    Add files via upload

---
+++ includes/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$con=mysqli_connect('localhost','root','','ecommerce');
+if(!$con){
+    die(mysqli_error($con));
+
+}
+
+?>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="bg-light p-2 ">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FAF0E6;">
+    <p>
+    VOUGE VENTURE - DESIGNED IN 2024
+    </p>
+</nav>
+</div>
